a29cec352ad54fbcd918ded72d48fa163bea6c6b
Luiz G. Galvão <llgalvao24@gmail.com>
2019-07-18 10:56:33 -0300

    EmployeeyController created + Read created

---
+++ src/main/java/net/guides/springboot2/springboot2jpacrudexample/employee/EmployeeController.java
@@ -0,0 +1,23 @@
+package net.guides.springboot2.springboot2jpacrudexample.employee;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RestController
+@CrossOrigin(origins = "http://localhost:4200")
+@RequestMapping("/api/v1")
+public class EmployeeController {
+
+    @Autowired
+    private EmployeeRepository employeeRepository;
+
+    @GetMapping("/employees")
+    public List<Employee> getAllEmployees(){
+        return employeeRepository.findAll();
+    }
+}
