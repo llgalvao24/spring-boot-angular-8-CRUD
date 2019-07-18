980320db461f6d13d5234b285e91ea8a3264d864
Luiz G. Galvão <llgalvao24@gmail.com>
2019-07-18 10:41:55 -0300

    entity Employee created

---
+++ src/main/java/net/guides/springboot2/springboot2jpacrudexample/employee/Employee.java
@@ -0,0 +1,27 @@
+package net.guides.springboot2.springboot2jpacrudexample.employee;
+
+import lombok.Data;
+import sun.util.resources.Bundles;
+
+import javax.persistence.*;
+
+@Data
+
+@Entity
+@Table(name = "employee")
+public class Employee {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(name = "first_name", nullable = false)
+    private String firstName;
+
+    @Column(name = "last_name", nullable = false)
+    private String lastName;
+
+    @Column(name = "email_address", nullable = false)
+    private String emailId;
+
+}
